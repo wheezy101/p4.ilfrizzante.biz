1a690f7ae4e3dd80d333cc581a307d1c2f9c0dba
Nina <nina.tobio@gmail.com>
2013-12-16 10:08:44 -0500

    first backup

---
+++ cleanmachine.js
@@ -0,0 +1,13 @@
+
+// Javascript for the chore timer
+
+var timeCircles = $(".choreTimer").TimeCircles({start: false});
+
+$(".start").click(function(){
+	$(".choreTimer").TimeCircles().start();
+	setTimeout(function(){alert("Time's up! Onto the next one.")},5000);
+	});
+$(".choreTimer").TimeCircles({count_past_zero: false});
+	
+
+// 
